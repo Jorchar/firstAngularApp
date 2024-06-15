f97e10b8ec3608c4ed1e40e4c4807966cb2475d9
jorchar <vixier61@gmail.com>
2024-06-15 12:05:45 +0200

    feature(sidemenu): Implement sidemenu More responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
 import { RouterModule } from "@angular/router";
 import {LoadingIndicatorComponent} from "./loading-indicator/loading-indicator.component";
+import {MatDrawerContainer, MatDrawerContent, MatDrawer} from "@angular/material/sidenav";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HomeComponent, RouterModule, LoadingIndicatorComponent],
+  imports: [HomeComponent, RouterModule, LoadingIndicatorComponent, MatDrawerContainer, MatDrawer, MatDrawerContent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
