156676066d2d456494ea3dfa593d2276cccb4869
jorchar <vixier61@gmail.com>
2024-06-16 13:47:28 +0200

    test(repair_default_tests): repaired default tests

--- src/app/plant-details/plant-details.component.spec.ts
+++ src/app/plant-details/plant-details.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlantDetailsComponent } from './plant-details.component';
+import {Router} from "@angular/router";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('PlantDetailsComponent', () => {
   let component: PlantDetailsComponent;
   let fixture: ComponentFixture<PlantDetailsComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlantDetailsComponent]
+      imports: [
+        PlantDetailsComponent,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
-    
+    router = TestBed.inject(Router);
+
     fixture = TestBed.createComponent(PlantDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
